430c27e6b3d2fad64da9cbea940f11e108513bba
Andres Predazzi <andres.predazzi@blockfi.com>
2021-09-21 19:54:14 -0300

    Changes in the way we handle the API

--- app/src/main/java/com/colopreda/theforktest/data/TheForkAPI.kt
+++ app/src/main/java/com/colopreda/theforktest/data/TheForkAPI.kt
@@ -1,9 +1,15 @@
 package com.colopreda.theforktest.data
 
+import com.colopreda.theforktest.BuildConfig
 import com.google.gson.JsonObject
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface TheForkAPI {
-    @GET("api?%20key=IPHONEPRODEDCRFV&method=restaurant_get_info&id_restaurant=6861")
-    suspend fun getRestaurantData(): RestaurantModels
+    @GET("api")
+    suspend fun getRestaurantData(
+        @Query("key") apiKey: String = BuildConfig.API_KEY,
+        @Query("method") method: String = "restaurant_get_info",
+        @Query("id_restaurant") restaurantId: String = "6861"
+    ): RestaurantModels
 }
